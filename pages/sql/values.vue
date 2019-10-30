53751e4a403051ed5d70b42c69e44901aac81656
hamada.y <hamada.y@egg.co.jp>
2019-10-30 19:26:53 +0900

    [master]-[update]: バグ、レイアウト修正

--- pages/sql/values.vue
+++ pages/sql/values.vue
@@ -9,16 +9,29 @@
                         v-model="target"
                         ></v-textarea>
                     
-                    <p>
-                        <v-radio-group v-model="delim_checked">
+                    <v-row align="center">
+                        <v-radio-group v-model="delim_checked" row>
                             <v-radio value="auto" label="自動"></v-radio>
-                            <v-radio v-for="item in delim_options" :value="item.value" :label="item.title"></v-radio>
-                            <v-radio value="" label="その他"></v-radio>
-                            <v-input v-show="delim_checked==''" v-model="delim_user" maxlength="1" />
+                            <v-radio v-for="item in delim_options" :value="item.value" :key="item.value">
+								<template v-slot:label>
+									{{ item.title }}
+									<code class="ml-1">{{ item.value }}</code>
+								</template>
+							</v-radio>
+                            <v-radio value="user" label="指定する"></v-radio>
                         </v-radio-group>
-                    </p>
+						<v-text-field
+							label="区切り文字"
+							hint="入力は1文字までです"
+							:disabled="delim_checked!='user'"
+							v-model="delim_user"
+							maxlength="1"
+							dense
+						></v-text-field>
+                    </v-row>
 
                     <v-textarea
+						label="結果"
                         v-model="result" 
                         filled
                         readonly
@@ -41,8 +54,8 @@ export default {
 		delim_options: [
 			{title:"カンマ", value:","},
 			{title:"タブ"  , value:"\t"},
-      {title:"セミコロン"  , value:";"},
-      {title:"バーティカルバー"  , value:"|"},
+			{title:"セミコロン"  , value:";"},
+			{title:"バーティカルバー"  , value:"|"},
 		],
 		delim_checked: "auto",
 		delim_user: "",
@@ -66,11 +79,10 @@ export default {
 		
 		delim () {
 			var self = this;
-			if (self.delim_checked == ""){ return self.delim_user; }
-			else if (self.delim_checked == "auto"){
-      	var sample = self.target.slice(0,200);
+			if (self.delim_checked == "auto" || (self.delim_checked == "user" && self.delim_user == "")){
+				var sample = self.target.slice(0,200);
 				var cnt = self.delim_options.map(e => {
-					var reg = new RegExp("\\x"+e.value.charCodeAt().toString(16), "g");
+					var reg = new RegExp("\\x"+("00"+e.value.charCodeAt().toString(16)).slice(-2), "g");
 					var mat = sample.match(reg)
 					if (mat){
 						return mat.length;
@@ -80,6 +92,9 @@ export default {
 					
 				});
 				return self.delim_options[cnt.indexOf(Math.max.apply(null,cnt))].value
+			}
+			else if (self.delim_checked == "user"){
+				return self.delim_user ? self.delim_user : ",";
 			}
 			return self.delim_checked;
 		},
